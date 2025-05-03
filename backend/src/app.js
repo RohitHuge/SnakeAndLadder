b146b5b499c3e8632457a1cc0d544c2a09529007
RohitHuge <rohitrhuge@gmail.com>
2025-05-04 05:00:18 +0530

    implement user registration with validation

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,13 +3,13 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 const app = express();
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
-
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
@@ -20,7 +20,7 @@ import userRouter from "./routes/user.routes.js"
 
 // router implementation
 
-app.use("/api/v1/user", userRouter);
+app.use("/api/v1/users", userRouter);
 
 
 export { app };
--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -1,6 +1,54 @@
 import { asyncHandler } from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/apiError.js";
+import { ApiResponse } from "../utils/apiResponse.js";
+import {User } from "../models/user.model.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
 
+const registerUser = asyncHandler(async (req, res) => {
+    // Validate request body
+    const { username, email, password } = req.body;
+    if([username, email].some(field => field?.trim() === "")) {
+        throw new ApiError(400, "Please fill all the fields");
+    }
 
-const registerUser = asyncHandler(async (req, res) => {res.status(200).json({message: "The Controller is working Rocks......!!!!"})})
+    const existedUser = await User.findOne({
+        $or: [
+            { username },
+            { email }
+        ]
+    });
+
+    if (existedUser) { 
+        console.log(User.findOne({ username }));
+        throw new ApiError(400, "User already exists");
+    }
+    /*
+    const avatarUrl = req.files?.avatar?.[0]?.path
+     if (!avatarUrl) {
+        throw new ApiError(400, "Please upload an avatar");
+    }
+    const avatar = await uploadOnCloudinary(avatarUrl);
+    if (!avatar) {
+        throw new ApiError(500, "Failed to upload avatar");
+    }
+    */
+    const user = await User.create({
+         username,
+         email,
+         password, 
+        // avatarUrl: avatar.url
+    });
+    
+    const createdUser = await User.findById(user._id).select("-password -refreshToken")
+
+    if (!createdUser) {
+        throw new ApiError(500, "Failed to create user");
+    }
+
+    return res.status(201).json(
+        new ApiResponse(201, createdUser, "User created successfully")
+    )
+   
+});
 
 export { registerUser }
