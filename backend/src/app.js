ff3b918ae54c452eaf1f28c8991e588961880903
RohitHuge <rohitrhuge@gmail.com>
2025-05-18 21:11:09 +0530

    fix: update CORS configuration to allow multiple frontend origins
    - Modified CORS settings in app.js to include 'https://snakeandladder.pages.dev/' alongside 'http://localhost:5173' for improved accessibility from different environments.
    - Updated Socket.IO CORS configuration to reflect the same changes, ensuring consistent behavior across the application.

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const httpServer = createServer(app);
 
 // CORS configuration
 app.use(cors({
-    origin: 'http://localhost:5173', // Your frontend URL
+    origin: ['http://localhost:5173','https://snakeandladder.pages.dev/'], // Your frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
@@ -21,7 +21,7 @@ app.use(cors({
 // Initialize Socket.IO with CORS configuration
 const io = new Server(httpServer, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: ['http://localhost:5173','https://snakeandladder.pages.dev/'],
         methods: ["GET", "POST"],
         credentials: true
     }
